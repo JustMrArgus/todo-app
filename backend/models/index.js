afe41408e720543d6240ead64977eee4e342264e
JustMrArgus <argusprogramming@protonmail.com>
2025-10-03 13:15:57 +0300

    Add todo model

---
+++ backend/models/index.js
@@ -0,0 +1,37 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "../db/db.sqlite"),
+  logging: false,
+});
+
+(async () => {
+  await sequelize.query("PRAGMA journal_mode = WAL;");
+})();
+
+const Todo = sequelize.define("Todo", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.ENUM("all", "done", "undone"),
+    allowNull: false,
+    defaultValue: "undone",
+  },
+  priority: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 1,
+      max: 10,
+    },
+  },
+  dueDate: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+});
+
+module.exports = { sequelize, Todo };
