6e06300ba094b3587856fd7dec64beb8c652cf78
JustMrArgus <argusprogramming@protonmail.com>
2025-10-03 13:16:45 +0300

    Connect sequelize to server

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ const port = process.env.APP_PORT || 3000;
 
 const bootstrap = async () => {
   try {
+    await sequelize.authenticate();
+    await sequelize.sync(); // temporary solution for test project
+    console.log("DB connected...");
     app.listen(port, () => console.log(`Server is listening on ${port}...`));
   } catch (err) {
     console.error("Server start error:", err);
